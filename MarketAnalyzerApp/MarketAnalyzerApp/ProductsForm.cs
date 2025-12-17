0309a9a4b30e08b226c79329066e0a4971ed1131
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-12-17 12:22:57 +0300

    🍵🍵🍵 grid appearance

--- MarketAnalyzerApp/MarketAnalyzerApp/ProductsForm.cs
+++ MarketAnalyzerApp/MarketAnalyzerApp/ProductsForm.cs
@@ -11,9 +11,34 @@ namespace MarketAnalyzerApp
         List<Product> _products; 
         private void ProductsForm_Load(object sender, EventArgs e)
         {
+           CustomizeGridAppearance();
             _products = CSVDataLoader.LoadData();
 
             grdProducts.DataSource = _products;
+        }
+
+        private void CustomizeGridAppearance()
+        {
+            grdProducts.AutoGenerateColumns = false;
+            grdProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            grdProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            grdProducts.RowHeadersVisible = false;
+
+            var titlecol = new DataGridViewTextBoxColumn {
+                HeaderText = "Title",
+                DataPropertyName = "Title",
+                SortMode = DataGridViewColumnSortMode.NotSortable
+            };
+            grdProducts.Columns.Add(titlecol);
+
+            var brandcol = new DataGridViewTextBoxColumn {
+
+                HeaderText = "Brand",
+                DataPropertyName = "Brand",
+                SortMode = DataGridViewColumnSortMode.NotSortable
+            };
+          
+            grdProducts.Columns.Add(brandcol);
         }
     }
 }
--- MarketAnalyzerApp/MarketAnalyzerApp/obj/Debug/net8.0-windows/MarketAnalyzerApp.AssemblyInfo.cs
+++ MarketAnalyzerApp/MarketAnalyzerApp/obj/Debug/net8.0-windows/MarketAnalyzerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketAnalyzerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f5ec234f94f5f94bffc2f9c1b9ca3495d0ebadd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076fe0778674a1454b7f7020654002312ccccede")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketAnalyzerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketAnalyzerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
